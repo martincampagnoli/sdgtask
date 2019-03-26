c08b7ea40c535644f07f80a18eece1a44a6aa731
Martin Campagnoli <mcampagnoli@canto.com>
2019-03-26 12:19:03 +0100

    Add elevators component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ElevatorsComponent } from './elevators/elevators.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElevatorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/elevators/elevators.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ElevatorsComponent } from './elevators.component';
+
+describe('ElevatorsComponent', () => {
+  let component: ElevatorsComponent;
+  let fixture: ComponentFixture<ElevatorsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ElevatorsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ElevatorsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
