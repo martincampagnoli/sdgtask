e298014ae1e5bc85f4af42d04daab940bec05df9
Martin Campagnoli <mcampagnoli@canto.com>
2019-03-26 12:23:19 +0100

    Add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ElevatorsComponent }      from './elevators/elevators.component';
+import { LoginComponent }      from './login/login.component';
+
+const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'elevators', component: ElevatorsComponent }
+  ];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {
+}
