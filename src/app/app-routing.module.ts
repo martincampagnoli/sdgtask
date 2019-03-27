6246b4d1656c7e17405e6f78af57c7b1cda4927f
Martin Campagnoli <mcampagnoli@canto.com>
2019-03-27 11:28:24 +0100

    Add functionality to login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,5 +13,6 @@ const routes: Routes = [
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+
 export class AppRoutingModule {
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    error = '';
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private authService: AuthService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
 
+        this.authService.logout();
+    }
+
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.authService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate(['elevators']);
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+                });
+    }
 }
