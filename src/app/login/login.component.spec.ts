5a19291f80d8cf285d0bae96305910b20784cb7b
Martin Campagnoli <mcampagnoli@canto.com>
2019-03-28 17:11:46 +0100

    Remove unused dependencies and add unit test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -31,4 +31,13 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should call the login function if the login button is clicked', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('.fourth');
+    button.click();
+    expect(component.submitted).toBeTruthy();
+  });
+
+
+
+
 });
--- src/app/services/elevators.service.spec.ts
+++ src/app/services/elevators.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ElevatorsService } from './elevators.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ElevatorsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: ElevatorsService = TestBed.get(ElevatorsService);
